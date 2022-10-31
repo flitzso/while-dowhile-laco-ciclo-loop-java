8cd90b4318d09a15f380d81bd4da807fd4ae9cd7
Flitzso <106411702+flitzso@users.noreply.github.com>
2022-10-31 16:20:56 -0300

    Add files via upload

---
+++ while-dowhile-laco-ciclo-loop-java/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int i = 1;
+        //while executa tudo que a dentro de parenteses puxando da variavel
+        //while(i < 101){
+            //System.out.println(i);
+            //com o ++i ele conta ate 100, sem o ++i ele percorre infinitamente
+            //++i;
+        //}
+
+        do{
+            System.out.println(i);
+            ++i;
+        }while(i < 10);
+    }
+}
